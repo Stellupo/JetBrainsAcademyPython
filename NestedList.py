a5b02f883947232c242b1d8ea0b51ab37f1bd638
Stellupo <e.braido@gmail.com>
2020-07-09 19:16:27 +0200

    Excercises on lists

--- NestedList.py
+++ NestedList.py
@@ -1,6 +1,7 @@
-#Let's write a program that will read a positive integer n from the input and create a nested
-#list containing the inner list [1, 2] repeated n times.
-n = int(input())
-number = [1,2]
-my_list = [number for x in range(n)]
-print(my_list)
+card_number = [4, 0, 0, 0, 0, 0, 9, 4, 1, 4, 6, 6, 9, 6, 9, 1]
+print(card_number)
+#card_number = ''.join(str(numbers) for numbers in card_number)
+print(card_number)
+print([card_number.index(x) for x in card_number])
+card_number = [2*x if ((card_number.index(x))%2)==0 else x for x in card_number]
+print(card_number)
