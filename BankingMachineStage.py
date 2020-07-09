c70a12cff4bdfe82d5dbf6dd57ccc95b9e7aed43
Stellupo <e.braido@gmail.com>
2020-07-09 19:21:52 +0200

    Banking Machine (stage 4)

--- BankingMachineStage.py
+++ BankingMachineStage.py
@@ -1,13 +1,12 @@
 import sqlite3
 import random
 
-# on ne doit le faire qu'une fois ! Pas chaque fois qu'une fonction est appelée
-
+# Creation database
 conn = sqlite3.connect('card.s3db')
 cur = conn.cursor()
 cur.execute('CREATE TABLE IF NOT EXISTS card (id INTEGER PRIMARY KEY, number TEXT, pin TEXT , balance INTEGER DEFAULT 0)')
 conn.commit()
-cur.execute('DROP TABLE card')  # réinitialisation de la table, pour vider la mémoire
+cur.execute('DROP TABLE card')  # reinitialisation de la table, pour vider la mémoire
 conn.commit()
 cur.execute('CREATE TABLE IF NOT EXISTS card (id INTEGER PRIMARY KEY, number TEXT, pin TEXT , balance INTEGER DEFAULT 0)')
 conn.commit()
@@ -17,6 +16,9 @@ class BankMachine:
         self.state_bank = "Home"
         self.clients_database = []
         self.start_menu()
+        self.balance = 0
+        self.card_number = []
+        self.code_pin = []
 
     def start_menu(self):
         print('''
@@ -28,7 +30,10 @@ class BankMachine:
     def account_menu(self):
         print('''
 1. Balance
-2. Log out
+2. Add income
+3. Do transfer
+4. Close account
+5. Log out
 0. Exit
 ''')
         self.state_bank = "Account"
@@ -47,39 +52,39 @@ class BankMachine:
 
     def create_account(self):
         while True:
-            card_number = [4, 0, 0, 0, 0, 0]
-            code_pin = []
+            self.card_number = [4, 0, 0, 0, 0, 0]
+            self.code_pin = []
             for i in range(10):  # on ajoute 9 chiffres pour avoir 15 chiffres dans le num carte
                 numbers = random.randint(0, 9)
-                card_number.append(numbers)
+                self.card_number.append(numbers)
             count = 0
             luhn_test = []
-            for x in card_number[:15]:
+            for x in self.card_number[:15]:
                 if count % 2 == 0:
                     luhn_test.append(2 * x)
                 elif count % 2 == 1:
                     luhn_test.append(x)
                 count += 1
             luhn_test = [y - 9 if y > 9 else y for y in luhn_test]
-            card_number_test = card_number[:15]
+            card_number_test = self.card_number[:15]
             card_number_test.extend([w for w in range(0, 9) if (sum(luhn_test) + w) % 10 == 0])
-            if card_number == card_number_test:
-                card_number = ''.join(str(numbers) for numbers in card_number)
-                if card_number not in self.clients_database:  # vérifier que le num soit unique
-                    self.clients_database.append(card_number)
+            if self.card_number == card_number_test:
+                self.card_number = ''.join(str(numbers) for numbers in self.card_number)
+                if self.card_number not in self.clients_database:  # vérifier que le num soit unique
+                    self.clients_database.append(self.card_number)
                     for i in range(4):
                         x = random.randint(0, 9)
-                        code_pin.append(x)
-                    code_pin = ''.join(str(numbers) for numbers in code_pin)
-                    self.clients_database.append(code_pin)
+                        self.code_pin.append(x)
+                    self.code_pin = ''.join(str(numbers) for numbers in self.code_pin)
+                    self.clients_database.append(self.code_pin)
                     print('\n' + 'Your card has been created')
-                    print('Your card number:', card_number, sep="\n")
-                    print('Your card PIN:', code_pin, sep="\n")
-                    self.database(card_number, code_pin)
+                    print('Your card number:', self.card_number, sep="\n")
+                    print('Your card PIN:', self.code_pin, sep="\n")
+                    self.database()
                     break
-                elif card_number in self.clients_database:
+                elif self.card_number in self.clients_database:
                     continue
-            elif card_number != card_number_test:
+            elif self.card_number != card_number_test:
                 continue
 
     def login(self):
@@ -99,20 +104,38 @@ class BankMachine:
                 self.start_menu()
 
     def user_account(self, user_choice):
-        balance = 0
         if user_choice == 1:
-            print("\n" + "Balance: ", balance)
+            print("\n" + "Balance: ", self.balance)
             self.account_menu()
-        elif user_choice == 2:
+        elif user_choice == 2:  # todo il va falloir ajouter la balance au registre de la database j'imagine
+            print('Enter income:')
+            deposit_money = int(input())
+            self.balance += deposit_money
+            print('Income was added!')
+            self.account_menu()
+        elif user_choice == 3:
+            print('Transfer', 'Enter card number:', sep='\n')
+            card_number = input()
+            if card_number in self.clients_database and card_number == self.card_number:
+                print("You can't transfer money to the same account!")
+            elif card_number in self.clients_database and card_number != self.card_number:
+                print('Enter how much money you want to transfer:')
+                transfer_money = int(input())
+                self.balance +=  #todo update !
+        elif user_choice == 4:
+            cur.execute('DELETE FROM card WHERE number = ?', (self.card_number, ))
+            print('The account has been closed!')
+            self.start_menu()
+        elif user_choice == 5:
             print("\n" + "You have successfully logged out!")
             self.start_menu()
         else:
             self.state_bank = "Account"
             print('Invalid answer')
 
-    def database(self, card_number, code_pin):
+    def database(self):
         req = 'INSERT INTO card (number, pin) VALUES (?,?)'
-        cur.execute(req, (card_number, code_pin))
+        cur.execute(req, (self.card_number, self.code_pin))  #
         conn.commit()
         cur.execute('SELECT * FROM card')
         table = cur.fetchall()
@@ -131,3 +154,5 @@ while True:
     except ValueError:
         print('Invalid answer')
         continue
+
+#todo cleaner la liste client_database et la remplacer par la base de données directement.
