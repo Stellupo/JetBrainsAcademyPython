29c2fcb1b1aeef248648dff73c36282d92b2b9e5
Stellupo <e.braido@gmail.com>
2020-07-14 17:50:54 +0200

    Numeric Matrix Processor (stage1)

--- NumericMatrixProcessor.py
+++ NumericMatrixProcessor.py
@@ -1,12 +1,13 @@
-def number_row_col(number_rows_column):
+def number_row_col(number_rows_column):  # liste clean du nombre de lignes,colonne sous cette forme [num, num]
     number_rows_column.remove(" ")
-    #print(number_rows_column)
+    # print(number_rows_column)
     row_count, col_count = int(number_rows_column[0]), int(number_rows_column[1])
-    #print(row_count)
-    #print(col_count)
+    # print(row_count)
+    # print(col_count)
     return row_count, col_count
 
-def matrice_user(row_count, col_count):
+
+def matrice_user(row_count, col_count):  # création d'une matrice à partir des inputs du users
     liste = []
     counter = 0
     while counter != row_count:
@@ -14,46 +15,35 @@ def matrice_user(row_count, col_count):
         liste += element
         liste = [x for x in liste if x != " "]
         counter += 1
-    '''for num in range(len(liste)):
-        liste[num] = int(liste[num])'''
-    matrice = creatMatrix(row_count, col_count, liste)
+    matrice = ([liste[i:i + col_count] for i in range(0, len(liste), col_count)])
     return matrice
 
-def creatMatrix(row_count, col_count, liste):
-    matrice=([liste[i:i+col_count] for i in range(0,len(liste),col_count)])
-    '''for i in range(row_count):
-        matrice.append([])
-        if col_count >= 2:
-            for j in range(col_count):
-                matrice[i].append(liste[row_count * i + j])  #liste[(row_count) * i + j])
-        elif col_count ==1:
-            matrice[i].append(liste[j])
-            j += 1
-        #matrice.append(rowList)'''
-    return matrice
 
-def matrice_add(matrice_1, matrice_2):
+def matrice_add(matrice_1, matrice_2):  # somme de deux matrices de même taille
     matrice_summed = []
     for num in range(len(matrice_1)):
         for i in range(len(matrice_1[0])):
             elements = str(int(matrice_1[num][i]) + int(matrice_2[num][i]))
             matrice_summed.append(elements)
-    #print(matrice_summed)
+    # print(matrice_summed)
     return matrice_summed
 
 
-#main
+# main
+# création de la matrice 1
 number_rows_column_1 = list(input())
 row_count, col_count = number_row_col(number_rows_column_1)
 matrice_1 = matrice_user(row_count, col_count)
-#print(matrice_1)
+# print(matrice_1)
+# création de la matrice 2
 number_rows_column_2 = list(input())
 row_count, col_count = number_row_col(number_rows_column_2)
 matrice_2 = matrice_user(row_count, col_count)
-#print(matrice_2)
+# print(matrice_2)
+# Somme des deux matrices conditionnellement à leurs tailles
 if number_rows_column_1 == number_rows_column_2:
-    matrice_summed = matrice_add(matrice_1,matrice_2)
-    matrice_summed = creatMatrix(row_count, col_count, matrice_summed)
+    matrice_summed = matrice_add(matrice_1, matrice_2)
+    matrice_summed = ([matrice_summed[i:i + col_count] for i in range(0, len(matrice_summed), col_count)])
     for num in range(len(matrice_summed)):
         print((' '.join(matrice_summed[num])))
 else:
