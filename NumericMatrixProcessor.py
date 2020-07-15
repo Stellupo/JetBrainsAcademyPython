3c05a735fbb33e0d3278b52eff75710e8508b4f4
Stellupo <e.braido@gmail.com>
2020-07-15 09:56:12 +0200

    Numeric Matrix Processor (stage1)

--- NumericMatrixProcessor.py
+++ NumericMatrixProcessor.py
@@ -1,9 +1,7 @@
-def number_row_col(number_rows_column):  # liste clean du nombre de lignes,colonne sous cette forme [num, num]
-    number_rows_column.remove(" ")
-    # print(number_rows_column)
-    row_count, col_count = int(number_rows_column[0]), int(number_rows_column[1])
-    # print(row_count)
-    # print(col_count)
+def number_row_col(number):  # liste clean du nombre de lignes,colonne sous cette forme [num, num]
+    number = number.split()
+    row_count, col_count = int(number[0]), int(number[1])
+    # print(row_count)  print(col_count)
     return row_count, col_count
 
 
@@ -11,8 +9,7 @@ def matrice_user(row_count, col_count):  # création d'une matrice à partir des
     liste = []
     counter = 0
     while counter != row_count:
-        element = list(input())
-        liste += element
+        liste += list(input())
         liste = [x for x in liste if x != " "]
         counter += 1
     matrice = ([liste[i:i + col_count] for i in range(0, len(liste), col_count)])
@@ -31,17 +28,18 @@ def matrice_add(matrice_1, matrice_2):  # somme de deux matrices de même taille
 
 # main
 # création de la matrice 1
-number_rows_column_1 = list(input())
-row_count, col_count = number_row_col(number_rows_column_1)
+
+input_1 = input() #list(input())
+row_count, col_count = number_row_col(input_1)
 matrice_1 = matrice_user(row_count, col_count)
 # print(matrice_1)
 # création de la matrice 2
-number_rows_column_2 = list(input())
-row_count, col_count = number_row_col(number_rows_column_2)
+input_2 = input()
+row_count, col_count = number_row_col(input_2)
 matrice_2 = matrice_user(row_count, col_count)
 # print(matrice_2)
 # Somme des deux matrices conditionnellement à leurs tailles
-if number_rows_column_1 == number_rows_column_2:
+if input_1 == input_2:
     matrice_summed = matrice_add(matrice_1, matrice_2)
     matrice_summed = ([matrice_summed[i:i + col_count] for i in range(0, len(matrice_summed), col_count)])
     for num in range(len(matrice_summed)):
