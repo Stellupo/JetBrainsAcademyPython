b7225913fe6f92475111aca3a57876eed7f23cb3
Stellupo <e.braido@gmail.com>
2020-08-05 17:48:20 +0200

    TicTacToe with AI : stage 4 (Suppression du terrain ramplacé par une liste, ajout du niveau medium, du menu démarrer avec 2 arguments)

--- TicTacToeAI.py
+++ TicTacToeAI.py
@@ -4,11 +4,8 @@ import random
 class TicTacToe:
     def __init__(self):
         self.reinitialisation()
-        #self.field_printing()
-
 
     def reinitialisation(self):
-        self.field = [[" ", " ", " "], [" ", " ", " "],[" ", " ", " "]]  # [[column for column in range(3)] for row in range(3)]x
         self.field_liste = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
         self.joueur_state = "X"
         self.state_game = set()
@@ -19,14 +16,11 @@ class TicTacToe:
 
     def field_printing(self): # affiche le terrain en table
         print("-" * 9)
-        print("|" + " " + self.field[0][2] + " " + self.field[1][2] + " " + self.field[2][2] + " " + "|")
-        print("|" + " " + self.field[0][1] + " " + self.field[1][1] + " " + self.field[2][1] + " " + "|")
-        print("|" + " " + self.field[0][0] + " " + self.field[1][0] + " " + self.field[2][0] + " " + "|")
+        print("|" + " " + self.field_liste[0] + " " + self.field_liste[1] + " " + self.field_liste[2] + " " + "|")
+        print("|" + " " + self.field_liste[3] + " " + self.field_liste[4] + " " + self.field_liste[5] + " " + "|")
+        print("|" + " " + self.field_liste[6] + " " + self.field_liste[7] + " " + self.field_liste[8] + " " + "|")
         print("-" * 9)
 
-    def update_fieldliste(self): # met a jour la liste des cases du terrain
-        self.field_liste = [elements for row in self.field for elements in row]
-        return self.field_liste
 
     def check_game(self): # verifie s'il y a un gagnant et stocke le résultat dans state_game
         # check diagonale principale
@@ -65,13 +59,11 @@ class TicTacToe:
             elif "_" in self.field_liste or " " in self.field_liste:
                 return ""
 
-    def game(self, row_coordinate, column_coordinate):  #
-        self.field[int(row_coordinate) - 1][int(column_coordinate) - 1] = self.joueur_state # place le pion
+    def game(self, position):  #
+        self.field_liste[position] = self.joueur_state # place le pion
         self.field_printing()  # affiche le terrain mis à jour
-        self.field_liste = self.update_fieldliste() # met a jour la liste des cases
         self.check_game() # verifie s'il y a un gagnant ou non
         self.result = self.state_game_func() # renvoie le resultat (Win, Draw ou Vide)
-        return self.result
 
     def user_move(self): # verifie que les coordonnees sont correctement rentrees
         move = input("Enter the coordinates: ").split(" ")
@@ -88,64 +80,66 @@ class TicTacToe:
                                                                                    4):  # vérifier qu'on ait les bons chiffres
                 print("Coordinates should be from 1 to 3!")
             else:
-                position = self.field[int(row_coordinate) - 1][int(column_coordinate)-1]
-                if position == "X" or position == "O":
+                position = (3 - column_coordinate) * 3 + (row_coordinate - 1)
+                if self.field_liste[position] == "X" or self.field_liste[position] == "O":
                     print("This cell is occupied! Choose another one!")
-                if position == "_" or position == " ":  # si la place est libre
-                    self.result = self.game(row_coordinate, column_coordinate)
-        return self.result
+                if self.field_liste[position] == "_" or self.field_liste[position] == " ":  # si la place est libre
+                    self.game(position)
+
+    def random_move(self):
+            while True:
+                row_coordinate, column_coordinate = random.randint(1, 3), random.randint(1, 3)
+                position = (3 - column_coordinate) * 3 + (row_coordinate - 1)
+                if self.field_liste[position] == "X" or self.field_liste[position] == "O":  # si la place est prise
+                    continue
+                elif self.field_liste[position] == "_" or self.field_liste[position] == " ":  # si la place est libre
+                    self.game(position)
+                    break
 
     def AI_move_easy(self):
         print('Making move level "easy"')
-        while True:
-            row_coordinate, column_coordinate = random.randint(1, 3), random.randint(1, 3)
-            position = self.field[int(row_coordinate) - 1][int(column_coordinate)-1]
-            if position == "X" or position == "O": # si la place est prise
-                continue
-            elif position == "_" or position == " ":  # si la place est libre
-                self.result = self.game(row_coordinate, column_coordinate)
-                break
-        return self.result
+        self.random_move()
 
     def AI_move_medium(self):
         print('Making move level "medium"')
+        AI_move = self.win_in_one_move()
+        if AI_move is None:
+            self.random_move()
+        else:
+            self.game(AI_move)
+
+
+    def win_in_one_move(self):
+        main_diag = [self.field_liste[0],self.field_liste[4],self.field_liste[8]]
+        diag_sec = [self.field_liste[2], self.field_liste[4], self.field_liste[6]]
         # check diagonale principale
-        main_diag = [self.field[0][2],self.field[1][1],self.field[2][0]]
-        if main_diag.count("X") == 2 or main_diag.count("O") == 2:
-            for m in range(3):
-                if self.field[m][2-m] == " " or self.field[m][2-m] == "_":
-                    self.game(m+1,2-m+1)
+        if (main_diag.count(" ") == 1 ) and (main_diag.count("X") == 2 or main_diag.count("O") == 2):
+            return main_diag.index(' ') * 4
         # check diagonale secondaire
-        diag_sec = [self.field[0][0],self.field[1][1],self.field[2][2]]
-        if diag_sec.count("X") == 2 or diag_sec.count("O") == 2:
-            for m in range(3):
-                if self.field[m][m] == " " or self.field[m][m] == "_":
-                    self.game(m+1,m+1)
+        if (diag_sec.count(" ") == 1 )and (diag_sec.count("X") == 2 or diag_sec.count("O") == 2):
+            return diag_sec.index(' ') * 2 + 2
         # check ligne
-        for i in range(3):
-            if self.field[i].count("X") == 2 or self.field[i].count("O")==2:
-                for j in range(3):
-                    if self.field[i][j] == "_"  or self.field[i][j]== " " :
-                            self.game(i+1,j+1)
+        rows = ((self.field_liste[0], self.field_liste[1], self.field_liste[2]),
+                 (self.field_liste[3], self.field_liste[4], self.field_liste[5]),
+                 (self.field_liste[6], self.field_liste[7], self.field_liste[8]))
+        for i in range(len(rows)):
+            if (rows[i].count(" ") == 1) and (rows[i].count("X") == 2 or rows[i].count("O") == 2):
+                return rows[i].index(' ') + (3 * i)
         # check colonne
-            column = [self.field[0][i],self.field[1][i],self.field[2][i]]
-            if column.count("X") == 2 or column.count("O") == 2 :
-                for k in range(3):
-                    if column[k] == "_" or column[k] == " ":
-                        self.game(k+1,i+1)
+        columns = ((self.field_liste[0], self.field_liste[3], self.field_liste[6]),
+                   (self.field_liste[1], self.field_liste[4], self.field_liste[7]),
+                   (self.field_liste[2], self.field_liste[5], self.field_liste[8]))
+        for j in range(len(columns)):
+            if (columns[j].count(" ") == 1) and (columns[j].count("X") == 2 or columns[j].count("O") == 2):
+                return columns[j].index(' ') * 3 + j
 
     def main_menu(self):
         while True:
             self.command = input('Input command: > ').split(' ')
             try: # on verifie qu'on ait tous les éléments de commande
                 if self.command[0] == 'start':
-                    if self.command[1] == 'easy' or self.command[2] == 'easy':
-                        if self.command[1] == 'user' and self.command[2] == 'easy':
-                            self.start(self.user_move, self.AI_move_easy)
-                        elif self.command[1] == 'easy' and self.command[2] == 'user':
-                            self.start(self.AI_move_easy, self.user_move)
-                        elif self.command[1] == 'easy' and self.command[2] == 'easy':
-                            self.start(self.AI_move_easy, self.AI_move_easy)
+                    if (self.command[1] in self.game_level or self.command[2] in self.game_level) or (self.command[1] in self.game_level and self.command[2] in self.game_level):
+                        self.gamelevel()
                     elif self.command[1] == self.command[2] == 'user':
                         self.start(self.user_move, self.user_move)
                     else:
@@ -160,26 +154,38 @@ class TicTacToe:
                 print('Bad parameters')
                 continue
 
+    def gamelevel(self):
+       if self.command[1] == 'easy' or self.command[2] == 'easy':
+            if self.command[1] == 'user' and self.command[2] == 'easy':
+                self.start(self.user_move, self.AI_move_easy)
+            elif self.command[1] == 'easy' and self.command[2] == 'user':
+                self.start(self.AI_move_easy, self.user_move)
+            elif self.command[1] == 'easy' and self.command[2] == 'easy':
+                self.start(self.AI_move_easy, self.AI_move_easy)
+            elif self.command[1] == 'easy' and self.command[2] == 'medium':
+                self.start(self.AI_move_easy, self.AI_move_medium)
+       if self.command[1] == 'medium' or self.command[2] == 'medium':
+           if self.command[1] == 'user' and self.command[2] == 'medium':
+               self.start(self.user_move, self.AI_move_medium)
+           elif self.command[1] == 'medium' and self.command[2] == 'user':
+               self.start(self.AI_move_medium, self.user_move)
+           elif self.command[1] == 'medium' and self.command[2] == 'medium':
+               self.start(self.AI_move_medium, self.AI_move_medium)
+           elif self.command[1] == 'medium' and self.command[2] == 'easy':
+               self.start(self.AI_move_medium, self.AI_move_easy)
+
     def start(self,func1, func2):
         self.field_printing()
         while self.result != 'stop':
-            if abs(self.field_liste.count("X") == self.field_liste.count("O")):
+            if int(self.field_liste.count("X")) == int(self.field_liste.count("O")):
                 self.joueur_state = 'X'
                 func1()
-            elif abs(self.field_liste.count("X") > self.field_liste.count("O")):
+            elif int(self.field_liste.count("X")) > int(self.field_liste.count("O")):
                 self.joueur_state = "O"
                 func2()
-
-
-
-
 
 
 game_instance = TicTacToe()
 
 # appel de la fonction principale
-
 game_instance.main_menu()
-#todo définir une fonction medium qui fasse réagir l'ordi selon la difficulté (2 pions)
-# intégrer dans la fonction Ai move la différence entre niveaux
-# segementer dans main menu pour plus de clarté
